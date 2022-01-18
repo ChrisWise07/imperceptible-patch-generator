00d56d0f80478fb4b7f3e71f978cc8af7afecb8e
ChrisWise07 <90012389+ChrisWise07@users.noreply.github.com>
2022-01-18 12:32:48 +1000

    Update custom_dpatch_robust.py

--- code/custom_dpatch_robust.py
+++ code/custom_dpatch_robust.py
@@ -260,7 +260,7 @@ class RobustDPatch(EvasionAttack):
                         y_batch = y[i_batch_start:i_batch_end]
 
                     # Sample and apply the random transformations:
-                    patched_images, patch_target, transforms, copy = self._augment_images_with_patch(
+                    patched_images, patch_target, transforms = self._augment_images_with_patch(
                         x[i_batch_start:i_batch_end], y_batch, self._patch, channels_first=self.estimator.channels_first
                     )
 
@@ -284,21 +284,12 @@ class RobustDPatch(EvasionAttack):
                 self._patch += self._old_patch_detection_update
 
             #update based on perceptibility
-            patched_image, patch_target, transforms, unpatched_image = self._augment_images_with_patch(
-                x[i_batch_start:i_batch_end], y_batch, self._patch, channels_first=self.estimator.channels_first
+            perc_patch_gradients = calculate_patch_perceptibility_gradients_no_trans(
+                self._patch, self.image_to_patch.image_rgb_diff, self.loss_tracker
             )
 
-            perc_patch_gradients = calculate_patch_perceptibility_gradients(
-                patched_image, unpatched_image, self.loss_tracker
-            )
-
-            perc_patch_gradients = self._untransform_gradients(
-                np.stack([perc_patch_gradients], axis=0), transforms, channels_first=self.estimator.channels_first
-            )
-
-            current_patch_perceptibility_update = perc_patch_gradients[0] * -(cosine_perceptibility_learning_rate)
+            current_patch_perceptibility_update = perc_patch_gradients * -(cosine_perceptibility_learning_rate)
             self._patch += current_patch_perceptibility_update
-
             #self._old_patch_perceptibility_update = np.add((self.perceptibility_momentum * self._old_patch_perceptibility_update), ((1 - self.perceptibility_momentum) * current_patch_perceptibility_update))
             #self._patch += self._old_patch_perceptibility_update
 
@@ -433,7 +424,7 @@ class RobustDPatch(EvasionAttack):
         if channels_first:
             x_patch = np.transpose(x_patch, (0, 3, 1, 2))
 
-        return x_patch, patch_target, transformations, x_copy
+        return x_patch, patch_target, transformations
 
     def _untransform_gradients(
         self,
