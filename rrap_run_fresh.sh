a52d85fc2e9e6c699a28ae75690f072b2d4ba10e
ChrisWise07 <90012389+ChrisWise07@users.noreply.github.com>
2022-01-20 11:50:44 +1100

    typos

--- rrap_run_fresh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rfd ./experiment_data/test
-python -W ignore ./code/main.py --data_folder_name 'test' --max_iter 1 --step_num 1 --patch_config "b" --decep_lr 0.00001 --percep_lr 0.0
