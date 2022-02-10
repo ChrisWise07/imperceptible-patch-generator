86aa5c7c01158703edb7acc8c29d47dec31809cc
ChrisWise07 <90012389+ChrisWise07@users.noreply.github.com>
2022-02-11 10:47:13 +1100

    Update execute_imperceptible_patch.sh

--- execute_imperceptible_patch.sh
+++ execute_imperceptible_patch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rfd ./experiment_data/test
-python -W ignore ./code/main.py --data_folder_name 'test' --max_iter 1 --step_num 1 --patch_config "is" --decep_lr 0.0001
+python -W ignore ./code/main.py --data_folder_name 'test' --max_iter 1000 --step_num 50
